6410a6066da4e3a584a815a12b70e39e0824e1b0
Soufyanbreda <soufyanb2@hotmail.com>
2019-03-10 11:43:39 +0100

    Add project

---
+++ js/script.js
@@ -0,0 +1,238 @@
+$(function(){
+	"use strict";
+	
+	var sect = $( window.location.hash ),
+		portfolio = $('.portfolio-items');
+	
+	if(sect.length == 1){
+		$('.section.active').removeClass('active');
+		sect.addClass('active');
+		if( sect.hasClass('border-d') ){
+			$('body').addClass('border-dark');
+		}
+	}
+	
+	/*=========================================================================
+		Magnific Popup (Project Popup initialization)
+	=========================================================================*/
+	$('.view-project').magnificPopup({
+		type: 'inline',
+		fixedContentPos: false,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+	
+	$(window).on('load', function(){
+		$('body').addClass('loaded');
+		
+		/*=========================================================================
+			Portfolio Grid
+		=========================================================================*/
+		portfolio.shuffle();
+		$('.portfolio-filters > li > a').on('click', function (e) {
+			e.preventDefault();
+			var groupName = $(this).attr('data-group');
+			$('.portfolio-filters > li > a').removeClass('active');
+			$(this).addClass('active');
+			portfolio.shuffle('shuffle', groupName );
+		});
+		
+	});
+	
+	/*=========================================================================
+		Navigation Functions
+	=========================================================================*/
+	$('.section-toggle').on('click', function(){
+		var $this = $(this),
+			sect = $( '#' + $this.data('section') ),
+			current_sect = $('.section.active');
+		if(sect.length == 1){
+			if( sect.hasClass('active') == false && $('body').hasClass('section-switching') == false ){
+				$('body').addClass('section-switching');
+				if( sect.index() < current_sect.index() ){
+					$('body').addClass('up');
+				}else{
+					$('body').addClass('down');
+				}
+				setTimeout(function(){
+					$('body').removeClass('section-switching up down');			
+				}, 2500);
+				setTimeout(function(){
+					current_sect.removeClass('active');
+					sect.addClass('active');
+				}, 1250);
+				if( sect.hasClass('border-d') ){
+					$('body').addClass('border-dark');
+				}else{
+					$('body').removeClass('border-dark');
+				}
+			}
+		}
+	});
+	
+	
+	/*=========================================================================
+		Testimonials Slider
+	=========================================================================*/
+	$('.testimonials-slider').owlCarousel({
+		items: 2,
+		responsive:{
+			992: {
+				items: 2
+			},
+			0: {
+				items: 1
+			}
+		}
+	});
+	
+	
+	
+	
+	
+	/*=========================================================================
+		Contact Form
+	=========================================================================*/
+	function isJSON(val){
+		var str = val.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, '');
+		return (/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(str);
+	}
+	$('#contact-form').validator().on('submit', function (e) {
+		
+		if (!e.isDefaultPrevented()) {
+			// If there is no any error in validation then send the message
+			
+			e.preventDefault();
+			var $this = $(this),
+				
+				//You can edit alerts here
+				alerts = {
+				
+					success: 
+					"<div class='form-group' >\
+						<div class='alert alert-success' role='alert'> \
+							<strong>Message Sent!</strong> We'll be in touch as soon as possible\
+						</div>\
+					</div>",
+					
+					
+					error: 
+					"<div class='form-group' >\
+						<div class='alert alert-danger' role='alert'> \
+							<strong>Oops!</strong> Sorry, an error occurred. Try again.\
+						</div>\
+					</div>"
+					
+				};
+			
+			$.ajax({
+			
+				url: 'mail.php',
+				type: 'post',
+				data: $this.serialize(),
+				success: function(data){
+					
+					if( isJSON(data) ){
+						
+						data = $.parseJSON(data);
+						
+						if(data['error'] == false){
+							
+							$('#contact-form-result').html(alerts.success);
+							
+							$('#contact-form').trigger('reset');
+							
+						}else{
+							
+							$('#contact-form-result').html(
+							"<div class='form-group' >\
+								<div class='alert alert-danger alert-dismissible' role='alert'> \
+									<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
+										<i class='ion-ios-close-empty' ></i> \
+									</button> \
+									"+ data['error'] +"\
+								</div>\
+							</div>"
+							);
+							
+						}
+						
+						
+					}else{
+						$('#contact-form-result').html(alerts.error);
+					}
+					
+				},
+				error: function(){
+					$('#contact-form-result').html(alerts.error);
+				}
+			});
+		}
+	});
+	
+	
+	
+});
+
+function mobdrop() {
+    var x = document.getElementById("header1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function scrollto (x) {
+  $('html, body').animate({
+    scrollTop: $("div#" + x).offset().top -110
+  }, 1000)
+};
+
+
+ function table1 (){
+$('#techtable1').show();
+$('#techtable2').hide();
+$('#techtable3').hide();
+$('#techtable4').hide();
+document.getElementById("link1").style.color = "#0078f3";
+document.getElementById("link2").style.color = "#000";
+document.getElementById("link3").style.color = "#000";
+document.getElementById("link4").style.color = "#000";
+ }
+function table2 (){
+$('#techtable1').hide();
+$('#techtable2').show();
+$('#techtable3').hide();
+$('#techtable4').hide();
+document.getElementById("link1").style.color = "#000";
+document.getElementById("link2").style.color = "#0078f3";
+document.getElementById("link3").style.color = "#000";
+document.getElementById("link4").style.color = "#000";
+}
+function table3 (){
+$('#techtable1').hide();
+$('#techtable2').hide();
+$('#techtable3').show();
+$('#techtable4').hide();
+document.getElementById("link1").style.color = "#000";
+document.getElementById("link2").style.color = "#000";
+document.getElementById("link3").style.color = "#0078f3";
+document.getElementById("link4").style.color = "#000";
+}
+function table4 (){
+$('#techtable1').hide();
+$('#techtable2').hide();
+$('#techtable3').hide();
+$('#techtable4').show();
+document.getElementById("link1").style.color = "#000";
+document.getElementById("link2").style.color = "#000";
+document.getElementById("link3").style.color = "#000";
+document.getElementById("link4").style.color = "#0078f3";
+}
+ 
